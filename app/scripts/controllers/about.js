dfb642e39e14898535e61b0c1ecb8d3f26b8e2b8
funnyling <funnyling444@gmail.com>
2015-06-22 16:52:38 +0300

    init commit 2

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name acmeshopApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the acmeshopApp
+ */
+angular.module('acmeshopApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name acmeshopApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the acmeshopApp
+ */
+angular.module('acmeshopApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
